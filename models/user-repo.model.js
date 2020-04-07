49eb5772c38b148c1c07f028902e9bae7bec2e76
Stefan Quest <stefan.quest@gmail.com>
2020-04-07 10:40:12 +0200

    commented out usermodels to allow push

--- models/user-repo.model.js
+++ models/user-repo.model.js
@@ -1,82 +1,82 @@
-const mongoose = require('mongoose');
-const bcrypt = require('bcrypt-nodejs');
+// const mongoose = require('mongoose');
+// const bcrypt = require('bcrypt-nodejs');
 
-const userSchema = mongoose.Schema({
-  local: {
-    username: {
-      type: String, unique: true, sparse: true, trim: true, lowercase: true
-    },
-    password: { type: String },
-    created: Date
-  },
-  profile: {
-    firstName: { type: String },
-    lastName: { type: String },
-    email: { type: String },
-    title: String,
-    avatar: String,
-    backgroundImage: String,
-    description: String,
-    profession: String,
-    genre: String,
-    keywords: [String],
-    address: String,
-    city: String,
-    extraInfo: String,
-    hyperlinks: [String],
-    facebookLink: String,
-    instagramLink: String,
-    youtubeLink: String,
-    label: String,
-    header: String,
-    grid: String,
-    post: String
-  },
-  site: {
-    name: {
-      type: String, unique: true, sparse: true, trim: true, lowercase: true
-    }
-  },
-  google: {
-    id: String,
-    token: String,
-    refreshToken: String,
-    username: String,
-    name: String,
-    sync: Boolean,
-    created: Date,
-    rawData: Object
-  },
-  facebook: {
-    id: String,
-    token: String,
-    longLivedToken: String,
-    username: String,
-    name: String,
-    sync: Boolean,
-    created: Date,
-    rawData: Object
-  },
-  instagram: {
-    id: String,
-    token: String,
-    username: String,
-    name: String,
-    sync: Boolean,
-    created: Date,
-    rawData: Object
-  }
-});
+// const userSchema = mongoose.Schema({
+//   local: {
+//     username: {
+//       type: String, unique: true, sparse: true, trim: true, lowercase: true
+//     },
+//     password: { type: String },
+//     created: Date
+//   },
+//   profile: {
+//     firstName: { type: String },
+//     lastName: { type: String },
+//     email: { type: String },
+//     title: String,
+//     avatar: String,
+//     backgroundImage: String,
+//     description: String,
+//     profession: String,
+//     genre: String,
+//     keywords: [String],
+//     address: String,
+//     city: String,
+//     extraInfo: String,
+//     hyperlinks: [String],
+//     facebookLink: String,
+//     instagramLink: String,
+//     youtubeLink: String,
+//     label: String,
+//     header: String,
+//     grid: String,
+//     post: String
+//   },
+//   site: {
+//     name: {
+//       type: String, unique: true, sparse: true, trim: true, lowercase: true
+//     }
+//   },
+//   google: {
+//     id: String,
+//     token: String,
+//     refreshToken: String,
+//     username: String,
+//     name: String,
+//     sync: Boolean,
+//     created: Date,
+//     rawData: Object
+//   },
+//   facebook: {
+//     id: String,
+//     token: String,
+//     longLivedToken: String,
+//     username: String,
+//     name: String,
+//     sync: Boolean,
+//     created: Date,
+//     rawData: Object
+//   },
+//   instagram: {
+//     id: String,
+//     token: String,
+//     username: String,
+//     name: String,
+//     sync: Boolean,
+//     created: Date,
+//     rawData: Object
+//   }
+// });
 
-userSchema.methods.generateHash = generateHash;
-userSchema.methods.validPassword = validPassword;
+// userSchema.methods.generateHash = generateHash;
+// userSchema.methods.validPassword = validPassword;
 
-module.exports = mongoose.model('User', userSchema);
+// module.exports = mongoose.model('User', userSchema);
 
-function generateHash(password) {
-  return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
-}
+// function generateHash(password) {
+//   return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
+// }
 
-function validPassword(password) {
-  return bcrypt.compareSync(password, this.local.password);
-}
+// function validPassword(password) {
+//   return bcrypt.compareSync(password, this.local.password);
+// }
