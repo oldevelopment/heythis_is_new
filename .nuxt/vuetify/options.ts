02ac3f77a86ff0310bbaa650c79fb24c17c57b29
Stefan Quest <stefan.quest@gmail.com>
2020-04-08 08:52:43 +0200

    Reverts to  "remove .nuxt folder and added to gitignore"
    This reverts commit 9acbfd7819ec0aaa271ea166544382504f1b1569.

---
+++ .nuxt/vuetify/options.ts
@@ -0,0 +1,23 @@
+import colors from 'vuetify/es5/util/colors';
+
+export default {
+  theme: {
+    themes: {
+      light: {
+        primary: '#AF3AFF',
+        background: {
+          page: '#f2f7fa',
+          portal: '#fff',
+        },
+      },
+      dark: {
+        primary: '#AF3AFF',
+        background: colors.shades.black,
+      },
+    },
+  },
+
+  icons: {
+    iconfont: 'mdi',
+  },
+};
